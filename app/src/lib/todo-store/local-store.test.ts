d96b23e585057cfc8552323c45097f057cf05171
Azat Valiev <azat@valievventures.com>
2024-03-08 15:56:39 -0600

    completed todos should still be relevant

--- app/src/lib/todo-store/local-store.test.ts
+++ app/src/lib/todo-store/local-store.test.ts
@@ -1,11 +1,16 @@
 import {
-  describe, beforeEach, it, expect,
+  describe,
+  beforeEach,
+  afterEach,
+  it,
+  expect,
+  vi,
 } from 'vitest';
 import { faker } from '@faker-js/faker';
 import { LocalTodoStore } from './local-store';
 import { TodoStatus, type CreateTodoInput } from './types';
 
-describe('Local browser store happy path', () => {
+describe('Basic CRUD', () => {
   let localTodoStore: LocalTodoStore;
 
   beforeEach(async () => {
@@ -20,7 +25,7 @@ describe('Local browser store happy path', () => {
     const insertedTodo = await localTodoStore.createTodo(todoContent);
 
     const { items: [todo, ...rest] } = await localTodoStore.getRelevantTodos(new Date());
-    expect(rest.length).toBe(0);
+    expect(rest).toHaveLength(0);
 
     expect(insertedTodo).toStrictEqual(insertedTodo);
     expect(todo).toBeDefined();
@@ -40,7 +45,7 @@ describe('Local browser store happy path', () => {
     });
 
     const { items: [todo, ...rest] } = await localTodoStore.getRelevantTodos(new Date());
-    expect(rest.length).toBe(0);
+    expect(rest).toHaveLength(0);
 
     expect(todo).toBeDefined();
     expect(todo!.status).toBe(TodoStatus.Complete);
@@ -57,7 +62,7 @@ describe('Local browser store happy path', () => {
     await localTodoStore.updateTodo({ id, content: newContent, status: TodoStatus.Complete });
 
     const { items: [updatedTodo, ...rest] } = await localTodoStore.getRelevantTodos(new Date());
-    expect(rest.length).toBe(0);
+    expect(rest).toHaveLength(0);
 
     expect(updatedTodo).toBeDefined();
     expect(updatedTodo!.updatedAt).toBeInstanceOf(Date);
@@ -67,5 +72,76 @@ describe('Local browser store happy path', () => {
       content: newContent,
       status: TodoStatus.Complete,
     }));
+  });
+});
+
+describe('Relevant Todos', () => {
+  const oneDayInMS = 24 * 60 * 60 * 1000;
+  const nowUnixTimestamp = Date.now();
+  let localTodoStore: LocalTodoStore;
+
+  beforeEach(async () => {
+    localTodoStore = await LocalTodoStore.create(faker.hacker.noun());
+    vi.useFakeTimers();
+    vi.setSystemTime(nowUnixTimestamp);
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
+  });
+
+  it('Does not include todo from previous days', async () => {
+    // Create a todo late yesterday
+    const yesterdayAlmostMidnight = new Date(nowUnixTimestamp - oneDayInMS);
+    yesterdayAlmostMidnight.setHours(23, 59, 59, 0);
+    vi.setSystemTime(yesterdayAlmostMidnight);
+
+    const yesterdayTodo = await localTodoStore.createTodo({
+      content: faker.lorem.sentence(),
+    });
+
+    // Create a todo very early this morning
+    const midnightThisMorning = new Date(nowUnixTimestamp);
+    midnightThisMorning.setHours(0, 0, 0, 0);
+    vi.setSystemTime(midnightThisMorning);
+
+    const todoCreatedMidnightThisMorning = await localTodoStore.createTodo({
+      content: faker.lorem.sentence(),
+    });
+    expect(
+      todoCreatedMidnightThisMorning.createdAt.valueOf(),
+    ).toEqual(midnightThisMorning.valueOf());
+
+    // Create todo at the end of day
+    const almostMidnightToday = new Date(nowUnixTimestamp);
+    almostMidnightToday.setHours(23, 59, 59, 0);
+    vi.setSystemTime(almostMidnightToday);
+
+    const todoCreatedAlmostMidnightTonight = await localTodoStore.createTodo({
+      content: faker.lorem.sentence(),
+    });
+    expect(
+      todoCreatedAlmostMidnightTonight.createdAt.valueOf(),
+    ).toEqual(almostMidnightToday.valueOf());
+
+    const { items: relevantTodos } = await localTodoStore.getRelevantTodos(new Date());
+    expect(relevantTodos).toHaveLength(2);
+    expect(relevantTodos, 'Yesterdays todo should not show up today').not.toContain(expect.objectContaining(yesterdayTodo));
+
+    expect(relevantTodos, 'Relevant todos should include everything created today').toStrictEqual(expect.arrayContaining([
+      todoCreatedMidnightThisMorning,
+      todoCreatedAlmostMidnightTonight,
+    ]));
+  });
+
+  it('completed todos are still relevant', async () => {
+    const completedTodo = await localTodoStore.createTodo({
+      content: faker.lorem.sentence(),
+    });
+
+    const { items: relevantTodos } = await localTodoStore.getRelevantTodos(new Date());
+    expect(relevantTodos).toHaveLength(1);
+
+    expect(relevantTodos[0]).toStrictEqual(completedTodo);
   });
 });
